e47900d52f7276824a7e594f9942db8f6fd45236
Lidia Santos <Lidia@Lidias-MacBook-Pro.local>
2016-01-03 18:08:49 -0600

    bootflix stuff

--- scripts/bootflixAjax.js
+++ scripts/bootflixAjax.js
@@ -8,11 +8,28 @@
 app.getMovieById = function getMovieById(id) {
 
   console.log("app.getMovieById() has been called. nothing happens. wait.. some tumbleweeds are tumbling by! an ID of '" + id + "' was entered.");
+  $.ajax({
+  type:'get',
+  url:'http://www.omdbapi.com/?i='+id+'&plot=full&r=json',
+  success:function(data){
+    console.log('huzzah getting by id works!')
+    console.log(data);
+    console.log(data.Title,data.Plot);
+    var movie = new app.MovieModel(data); //new MovieModel object
+    var view = new app.MovieView(data); //new MovieView object
+    view.render();
+    console.log(movie.Title);
+  },
+  error: function(err){
+    console.log(err);
+  }
+});//end ajax call
 
   // request URL for omdb's id search
   // http://www.omdbapi.com/?i=tt0095016&plot=full&r=json
 
   // 1. create your ajax request and then in your success method.
+
   // 2. you should create a new MovieModel object based on the returned
   // result.
   // var movie = new app.MovieModel(data);
@@ -34,12 +51,27 @@ app.getMovieByTitle = function getMovieByTitle(title) {
   //http://www.omdbapi.com/?t=Die+Hard&y=1988&plot=full&r=json
 
   // 1. create your ajax request and then in your success method.
-  // 2. you should create a new MovieModel object based on the returned
-  // result.
-  // var movie = new app.MovieModel(data);
-  // 3. you should create a new MovieView object based on movie model
-  // 4. you call render() on the view
-  // 5. your render() should append the `$el` to the DOM
+  $.ajax({
+   type:'get',
+   url:'http://www.omdbapi.com/?t='+title+'&y=&plot=full&r=json',
+   success:function(data){
+     console.log('huzzah get by title works!')
+     console.log(data);
+     console.log(data.imdbID);
+     $('#movie-listing').append("<img src='http://img.omdbapi.com/?i='+data.imdbID+'&apikey=d31f1a94 ' alt=data.title");
+     var movie = new app.MovieModel(data);
+     var view = new app.MovieView(movie);
+     view.render();
+
+     console.log(data);
+     console.log(view.Title);
+     console.log(movie.imdbRating);
+   },
+   error:function(err){
+     console.log(err);
+   }
+ });
+
 
 
 }
@@ -52,6 +84,17 @@ app.getMovieByTitle = function getMovieByTitle(title) {
  */
 app.MovieModel = function MovieModel(options) {
 
+    this.Title=options.Title;
+    this.Year=options.Year;
+    this.Rated=options.Rated;
+    this.Released=options.Released;
+    this.Genre=options.Genre;
+    this.Plot=options.Plot;
+    this.Poster=options.Poster;
+    this.Director=options.Director;
+    this.Year=options.Year;
+    this.imdbRating=options.imdbRating;
+    this.imdbID=options.imdbID;
   // id, title, rating, director, plot, year, genre should all be in the `options` object
   // store all the information in the model
 
@@ -63,7 +106,6 @@ app.MovieModel = function MovieModel(options) {
  * @param options  - options object
  */
 app.MovieView = function MovieView(options) {
-
   // options should contain the `model` for which the view is using
 
   // 1. create a view
@@ -72,5 +114,21 @@ app.MovieView = function MovieView(options) {
   //    you will want to add the id, title, rating, director, plot, year,
   //    and genre. See design/movielayout.html
   // 4. finally, render() will $(selector).append() each new '.movie' to "#movie-listing".
+
+  this.imdbID=options.imdbID;
+  this.Title=options.Title;
+  this.imdbRating=options.imdbRating;
+  this.Director=options.Director;
+  this.Plot=options.Plot;
+  this.Year=options.Year;
+  this.Genre=options.Genre;
+  this.Poster=options.Poster;
+  this.render=function(){
+    var div = $('<div class="movie"><table><tr><td><img src="'+this.Poster+'" alt="'+this.Title+'""></td><td><h3>'+this.Title+'</h3><p><strong>Released:</strong>'+this.Year+'<br><strong>Directed By:</strong>'+this.Director+'<br><em>'+this.Genre+'</em></p><p>'+this.Plot+'</p></td></tr></table></div>');
+
+  $('#movie-listing').append(div);
+
+
+}
 
 }
